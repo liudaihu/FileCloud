3b576ff9cf9e95068218cbc6cc868523e7b4728b
Artem Isaev <artem.isaev.it@gmail.com>
2021-05-09 22:13:13 +0300

    Add exact file downloading

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import *
-from file_manager import files
+from file_manager import files, file_indexes
 
 app = Flask(__name__)
 
@@ -9,8 +9,22 @@ def main_page():
 
 @app.route("/files")
 def files_page():
-    return render_template("files.html", files=files)
+    return render_template("files.html", file_indexes=file_indexes, files=files)
 
 @app.route("/files/download_all")
 def download_all():
-    return send_file("user_files/files.zip", as_attachment=True)
+    return send_file("user_files/files.zip", as_attachment=True, cache_timeout=-1)
+
+@app.route("/files/<int:file_id>")
+def download_file(file_id):
+    if file_id in file_indexes:
+        return send_file(f"user_files/{files[file_id]}", as_attachment=True, cache_timeout=-1)
+    return 'Error 404: Page not found'
+
+# stop caching files
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
--- file_manager.py
+++ file_manager.py
@@ -5,8 +5,16 @@ try:
     os.chdir("user_files")
 except:
     os.makedirs("user_files")
+    os.chdir("user_files")
+
+if "files.zip" in os.listdir():
+    os.remove("files.zip")
 
 files = os.listdir()
+
+file_indexes = []
+for i in range(len(files)):
+    file_indexes.append(i)
 
 archive = ZipFile('files.zip', 'w')
 
